f3ab961a5a8b730b251b14ad1e8977a22abb5b22
Alpri Else <alprielse@gmail.com>
2017-05-11 08:57:55 -0500

    Removed Angular JS... will replace with React

--- public/js/app.js
+++
@@ -1,8 +0,0 @@
-(function() {
-    var app = angular.module('maineDashboard', []);
-
-    app.controller('InfoController', ['$scope', function($scope) {
-        $scope.time = moment().format('hh:mm');
-        $scope.date = moment().format('dddd MMMM Do, YYYY');
-    }]);
-}());
