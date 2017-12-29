b6f78646f58435ed5b5f1c4b186a25266245799c
Alpri Else <alprielse@gmail.com>
2017-12-29 01:05:57 -0600

    Setup api

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
 
+//  fs is for temp data, replace with db connection when setup
+var fs = require('fs');
+var path = require('path');
 
-router.get('/', function(req, res) {
-    res.send("hi");
+router.get('/schedule', function(req, res) {
+  //  Insert Logic to figure out which schedule to serve
+
+  //  Replace with database calls
+  fs.readFile(path.join(__dirname,"./temp.json"), function (err, data) {
+    if (err) {
+      console.log(process.cwd());
+    };
+    res.write(data);
+  });
 });
-
 module.exports = router;
