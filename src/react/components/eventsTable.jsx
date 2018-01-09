1614516d5055d79dfef3a42e9a71e6cf79619cfc
Alpri Else <alprielse@gmail.com>
2018-01-08 19:58:10 -0600

    Progress

--- src/react/components/eventsTable.jsx
+++ src/react/components/eventsTable.jsx
@@ -10,31 +10,34 @@ export default class EventsTable extends React.Component {
   }
   componentWillReceiveProps() {
     var events = this.props.events;
-    events.sort((a, b) => {
-      var startA = moment(a.start, "HH:mm");
-      var startB = moment(b.start, "HH:mm");
-      if(startA.isBefore(startB)) {
-        return -1;
-      } else {
-        return 1;
-      }
-    });
+    var eventsArr;
+    if (events.length > 0) {
+      events.sort((a, b) => {
+        var startA = moment(a.start, "HH:mm");
+        var startB = moment(b.start, "HH:mm");
+        if(startA.isBefore(startB)) {
+          return -1;
+        } else {
+          return 1;
+        }
+      });
+      eventsArr = events.map(event => {
+        var start = moment(event.start, "HH:mm").format("hh:mm");
+        var end = moment(event.end, "HH:mm").format("hh:mm");
+        var time = start + " - " + end;
+        var key = "event_" + event.name + start + end
+        return (
+          <tr key={key}>
+            <td>{time}</td>
+            <td>{event.name}</td>
+            <td><button type="button" name={key} className="btn btn-danger" onClick={this.props.handleDeleteEvent}>Delete Event</button></td>
+          </tr>
+        )
 
-    var eventsArr = events.map(event => {
-      var start = moment(event.start, "HH:mm").format("hh:mm");
-      var end = moment(event.end, "HH:mm").format("hh:mm");
-      var time = start + " - " + end;
-      var key = "event_" + event.name + start + end
-      return (
-        <tr key={key}>
-          <td>{time}</td>
-          <td>{event.name}</td>
-          <td><button type="button" name={key} className="btn btn-danger" onClick={this.props.handleDeleteEvent}>Delete Event</button></td>
-        </tr>
-      )
-
-    });
-
+      });
+    } else {
+      eventsArr = <tr><td>No events scheduled.</td></tr>;
+    }
     this.setState({
       events: eventsArr
     });
